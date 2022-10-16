0aa4b5cfccd5cdb08026095445dc7ed521e50a1e
Murad <49407545+Murad14@users.noreply.github.com>
2022-10-16 21:34:50 +0600

    minor change done

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def play_game():
         computer_score = calculate_score(computer_cards)
 
         print(f"  Your cards: {user_cards}, current score: {user_score}")
-        print(f"  Computer's first card: {computer_cards[0]}")
+        print(f"  Opponent's first card: {computer_cards[0]}")
 
         if user_score == 0 or computer_score == 0 or user_score > 21:
             is_game_over = True
